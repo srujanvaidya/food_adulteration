d1b0b28186ddb3e7dacd00179c8b75a26e2ce911
srujan <vaidyasrujan@gmail.com>
2025-10-05 12:55:47 +0530

    hosting

--- api1/index.py
+++ api1/index.py
@@ -1,11 +1,10 @@
-import sys
 import os
+import sys
 
-# Add your project path so Python can find settings.py
+# Add Django project path
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'adultration'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adultration_main.settings')
 
 from django.core.wsgi import get_wsgi_application
-
 app = get_wsgi_application()
